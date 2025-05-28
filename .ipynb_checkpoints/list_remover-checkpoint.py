e4d0860cdfef98c05da804819ed1e75182d7a888
lyszt <kalliddel@gmail.com>
2025-05-28 16:43:21 -0300

    Initial commit

---
+++ .ipynb_checkpoints/list_remover-checkpoint.py
@@ -0,0 +1,21 @@
+import random
+from IPython.display import display, Markdown, Latex, clear_output, display_markdown
+num_list = []
+for i in range(10):
+    rn = random.randint(0,20)
+    while rn in num_list:
+        rn = random.randint(0,20)
+    num_list.append(rn)
+num = str(num_list).replace("[","").replace("]","")
+display(Markdown((f"# ``` {num} ```" )))
+
+for i in range(0, len(num_list)-1):
+    ch = int(input())
+    while(ch not in num_list or ch == -1):
+        ch = int(input())
+    if ch == -1:
+        sys.exit(0)
+    num_list[i] = "x"
+    num = str(num_list).replace("[","").replace("]","")
+    clear_output()
+    display(Markdown((f"# ``` {num} ```" )))
